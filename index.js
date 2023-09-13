e3c16fa961d066a77ddc2e96dd51a04eec8e9a14
MuhammadSaqibShoaib <muhammadsaqib54@outlook.com>
2023-09-13 21:05:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ app.post('/download', async (req, res) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        return res.status(200).json({ data: imageStream.data });
+        return res.send(imageStream.data);
     } catch (error) {
         console.log(error)
         return res.status(500).json({ message: error.message });
