05a7ffc2efb8f171fbebc31c9d13e187dbb0bedf
MuhammadSaqibShoaib <muhammadsaqib54@outlook.com>
2023-09-27 16:26:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ console.log('WebSocket server is running on port 3000');
 
 app.get("/checksocket",(req,res)=>{
   try{
-  console.log(wss)
+  console.log(wss.path)
   return res.send("OK")
   }
   catch(error){
